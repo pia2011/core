68dcbe5e8c16674989b98d0e7775fdd0ff125ab4
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-06-14 13:59:00 +0900

    싱글톤 방식 : stateful

--- src/test/java/study/spring/core/singleton/StatefulService.java
+++ src/test/java/study/spring/core/singleton/StatefulService.java
@@ -2,15 +2,16 @@ package study.spring.core.singleton;
 
 public class StatefulService {
 
-    private int price; // 상태유지 필드 : stateful
+//    private int price; // 상태유지 필드 : stateful
 
-    public void order(String name, int price){
+    public int order(String name, int price){
         System.out.println("name = " + name + "price = " + price);
-        this.price = price;
-    }
-
-    public int getPrice(){
+//        this.price = price;
         return price;
     }
+
+//    public int getPrice(){
+//        return price;
+//    }
 
 }
