dcd516f834fc2f44fa2e2bfaf660c9f4f7ac63a2
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-06-20 10:26:41 +0900

    필드 주입의 예시(편리하지만 사용하면 안됨 : 순수 자바로 테스트 불가능)

--- src/main/java/study/spring/core/order/OrderServiceImpl.java
+++ src/main/java/study/spring/core/order/OrderServiceImpl.java
@@ -9,8 +9,8 @@ import study.spring.core.member.MemberRepository;
 @Component
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+    @Autowired private MemberRepository memberRepository;
+    @Autowired private DiscountPolicy discountPolicy;
 
 //    private MemberRepository memberRepository;
 //    private DiscountPolicy discountPolicy;
@@ -25,11 +25,11 @@ public class OrderServiceImpl implements OrderService{
 //        this.discountPolicy = discountPolicy;
 //    }
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
