469e4b4bae21a9d4971259d8530337ef6638b461
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-06-20 10:27:35 +0900

    Rollback

--- src/main/java/study/spring/core/order/OrderServiceImpl.java
+++ src/main/java/study/spring/core/order/OrderServiceImpl.java
@@ -9,27 +9,14 @@ import study.spring.core.member.MemberRepository;
 @Component
 public class OrderServiceImpl implements OrderService{
 
-    @Autowired private MemberRepository memberRepository;
-    @Autowired private DiscountPolicy discountPolicy;
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
-//    private MemberRepository memberRepository;
-//    private DiscountPolicy discountPolicy;
-//
-//    @Autowired(required = false) // 주입할 대상이 없어도 동작
-//    public void setMemberRepository(MemberRepository memberRepository){
-//        this.memberRepository = memberRepository;
-//    }
-//
-//    @Autowired
-//    public void setDiscountPolicy(DiscountPolicy discountPolicy){
-//        this.discountPolicy = discountPolicy;
-//    }
-
-//    @Autowired
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
