0cacf51bd20ae8aebb11638863865ef59d2153cc
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-06-30 18:09:08 +0900

    request 스코프 빈을 이용해서 Logger 만들기

---
+++ src/main/java/study/spring/core/web/LogDemoService.java
@@ -0,0 +1,18 @@
+package study.spring.core.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+import study.spring.core.common.MyLogger;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+
+    public void logic(String id) {
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
+        myLogger.log("service id = "+id);
+    }
+}
